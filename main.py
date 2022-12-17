34e2bae5c6979f745a994e2f57838b5e3aadccfc
Vadim Akhmetov <vaakhmetov@ozon.ru>
2022-12-17 12:42:44 +0300

    add his mom

--- main.py
+++ main.py
@@ -12,10 +12,14 @@ def print_hi(name):
 def print_hi_surname(surname):
     print(f'Hi, {surname}')
 
+def print_hi_mom():
+    print('Hi, mom')
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
     print('Hello new git branch')
     print_hi_surname('Akhmetov')
+    print_hi_mom()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
